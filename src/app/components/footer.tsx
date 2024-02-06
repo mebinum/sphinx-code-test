4ae2e2a6dd5d4a734d7a1e98baa083d2211656f8
Mike Ebinum <mebinum@gmail.com>
2024-02-06 12:08:02 +1100

    updated footer to open in new page

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -31,6 +31,7 @@ export default function Footer() {
             <a
               key={item.name}
               href={item.href}
+              target="_blank"
               className="text-gray-400 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
